7397b16f9d112c0a875ba87c604f3e9430cebe1a
Mariana Plauska <103209930+MarianaPlauska@users.noreply.github.com>
2024-11-26 12:22:07 -0300

    feature: adicionando arquivos iniciais spring

---
+++ src/main/java/br/com/mural/criando/futuro/MuralCriandoOFuturoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mural.criando.futuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralCriandoOFuturoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuralCriandoOFuturoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mural/criando/futuro/MuralCriandoOFuturoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mural.criando.futuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralCriandoOFuturoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
